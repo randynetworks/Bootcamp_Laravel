31dfbd50822750a636f8b39f1cb55baedc244bee
randynetworks <randynetworks@gmail.com>
2020-06-24 20:14:24 +0700

    menyelesaikan ubah huruf

--- week2day3PHP3/ubah-huruf.php
+++ week2day3PHP3/ubah-huruf.php
@@ -1,6 +1,13 @@
 <?php
-function ubah_huruf($string){
-//kode di sini
+function ubah_huruf($string)
+{
+    //kode di sini
+    $i = 0;
+    while ($i < strlen($string)) {
+        $string[$i] = chr(ord($string[$i]) + 1);
+        $i++;
+    }
+    return $string . "<br>";
 }
 
 // TEST CASES
@@ -9,5 +16,3 @@ echo ubah_huruf('developer'); // efwfmpqfs
 echo ubah_huruf('laravel'); // mbsbwfm
 echo ubah_huruf('keren'); // lfsfo
 echo ubah_huruf('semangat'); // tfnbohbu
-
-?>
