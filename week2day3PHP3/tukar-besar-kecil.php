c449e1f252fd23cfc2b8a1620676d6aff4bc8038
randynetworks <randynetworks@gmail.com>
2020-06-25 05:51:02 +0700

    menyelesaikan tukar huruf

--- week2day3PHP3/tukar-besar-kecil.php
+++ week2day3PHP3/tukar-besar-kecil.php
@@ -1,6 +1,20 @@
 <?php
-function tukar_besar_kecil($string){
-//kode di sini
+function tukar_besar_kecil($string)
+{
+    //kode di sini
+    $i = 0;
+    while ($i < strlen($string)) {
+        $valAscii = ord($string[$i]);
+        if ($valAscii >= 65 && $valAscii <= 90) {
+            $string[$i] = chr($valAscii + 32);
+        }
+        if ($valAscii >= 97 && $valAscii <= 122) {
+            $string[$i] = chr($valAscii - 32);
+        }
+
+        $i++;
+    }
+    return "$string<br>";
 }
 
 // TEST CASES
@@ -9,5 +23,3 @@ echo tukar_besar_kecil('I aM aLAY'); // "i Am Alay"
 echo tukar_besar_kecil('My Name is Bond!!'); // "mY nAME IS bOND!!"
 echo tukar_besar_kecil('IT sHOULD bE me'); // "it Should Be ME"
 echo tukar_besar_kecil('001-A-3-5TrdYW'); // "001-a-3-5tRDyw"
-
-?>
