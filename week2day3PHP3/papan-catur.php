e3c6382d91e2044e2419d1b6f08fd1c42bbf5954
randynetworks <randynetworks@gmail.com>
2020-06-25 07:17:26 +0700

    done papan catur

--- week2day3PHP3/papan-catur.php
+++ week2day3PHP3/papan-catur.php
@@ -1,11 +1,42 @@
 <?php
 
-function papan_catur($angka) {
-// tulis kode di sini
+function papan_catur($angka)
+{
+    // tulis kode di sini
+
+    $board = '';
+    $side = $angka;
+
+    $board .= '<table>';
+
+    $i = 0;
+    while ($i < $side) {
+        $board .= '<tr>';
+        $length = $side * 2 - 1;
+
+        $j = 0;
+        while ($j < $length) {
+            $isi = '';
+
+            if (($i % 2 == 0 && $j % 2 == 0) || ($i % 2 == 1 && $j % 2 == 1)) {
+                $isi = '#';
+            } else
+                $isi = ' ';
+            $board .= '<td>' . $isi . '</td>';
+
+            $j++;
+        }
+
+        $i++;
+    }
+    $board .= '</tr>';
+    // Tag Penutup Table
+    $board .= '</table><br>';
+    return $board;
 }
 
 // TEST CASES
-echo papan_catur(4) ;
+echo papan_catur(4);
 /*
 # # # #
  # # #
