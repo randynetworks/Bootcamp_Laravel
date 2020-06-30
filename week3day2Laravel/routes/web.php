5beb19e4259c02213bc43f870eaa47778a5da410
randynetworks <randynetworks@gmail.com>
2020-06-30 11:38:12 +0700

    menyelesaikan controller dan views

--- week3day2Laravel/routes/web.php
+++ week3day2Laravel/routes/web.php
@@ -2,17 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// HOME
+Route::get('/', 'HomeController@index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// REGISTER
+Route::get('/register', 'AuthController@register');
+
+// WELCOME
+Route::get('/welcome', 'AuthController@welcome');
