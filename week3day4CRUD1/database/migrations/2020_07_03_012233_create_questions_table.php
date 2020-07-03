7628952ac8552f6a5dd5f5b30c1be1386e887e0a
randynetworks <randynetworks@gmail.com>
2020-07-03 08:44:57 +0700

    membuat read pertanyaan

---
+++ week3day4CRUD1/database/migrations/2020_07_03_012233_create_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('isi');
+            $table->integer('like');
+            $table->integer('dislike');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
---
+++ week3day4CRUD1/resources/views/forum.blade.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title><?= $title; ?></title>
+    <style>
+        body {
+            background: rgba(238, 238, 238, 1);
+        }
+
+        .card-deck {
+            background: rgba(250,250,250,1);
+            border-radius: 15px;
+        }
+        .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-12">
+                <div class="mt-3 card-deck row m-0 justify-content-center shadow">
+                    <div class="card-body text-center">
+                        <h1 class="p-3 display-4">Larahub | Forum</h1>
+                        <div class="links">
+                            <a href="{{url('/')}}">Home</a>
+                            <a href="{{url('/pertanyaan')}}">Forum</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-8">
+                <div class="mt-3 card-deck row m-0 justify-content-center shadow">
+                    <div class="card-body">
+                        <h3>Daftar Pertanyaan.</h3><hr>
+                        @foreach ($questions as $question)
+
+                        <h5>{{$question->judul}}</h5>
+                        <p>{{$question->isi}}</p>
+                            <a href="" class="btn btn-success">Jawab Pertanyaan</a>
+                            <hr>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="mt-3 card-deck row m-0 justify-content-center shadow">
+                    <div class="card-body">
+                        <h3>Buat Pertanyaan.</h3>
+                        <form>
+                            <div class="form-group">
+                              <label for="judul">Judul Pertanyaan</label>
+                              <input type="text" class="form-control" id="judul" name="judul" placeholder="Masukan Judul Pertanyaan">
+                            </div>
+                            <div class="form-group">
+                              <label for="isi">Isi Pertanyaan</label>
+                              <textarea type="text" class="form-control" id="isi" name="isi" placeholder="Masukan Pertanyaan kamu!"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                          </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
